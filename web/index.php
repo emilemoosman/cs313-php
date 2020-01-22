c777b688b27347f4f9b40f8668cdc74b5209546e
emilemoosman <emilemoosman@byui.edu>
2020-01-23 00:28:38 +1100

    add more bootstrap #3

--- web/index.php
+++ web/index.php
@@ -33,24 +33,9 @@
     <div class="col-sm-4">
       <h2>Beautiful Sydney:</h2>
       <div class="sydney"></div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <p>picturesque Darling Harbour.</p>
       <h3>Some Links</h3>
-      <p>Lorem ipsum dolor sit ame.</p>
-      <ul class="nav nav-pills flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <hr class="d-sm-none">
+      
     </div>
     <div class="col-sm-8">
       <h2>TITLE HEADING</h2>
