35312928849080ef97d985be01040366e3f9646b
emilemoosman <emilemoosman@byui.edu>
2020-01-23 01:04:16 +1100

    add more bootstrap #11

--- web/index.php
+++ web/index.php
@@ -24,28 +24,28 @@
     <h1>Emile Moosman Home Page</h1> 
   </div>
 
-  <nav class="navbar">
+  <nav class="navbar">sss
 
   </nav>
 
   <div class="container" style="margin-top:30px">
-  <div class="row">
-    <div class="col-sm-4">
-      <h2>Beautiful Sydney:</h2>
-      <div class="sydney"></div>
-      <p>Picturesque Darling Harbour.</p>
-      
-    </div>
-    <div class="col-sm-8">
-      <h2>About Me:</h2>
-      <h5>Title description, Dec 7, 2017</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-      <br>
+    <div class="row">
+      <div class="col-sm-4">
+        <h2>Beautiful Sydney:</h2>
+        <div class="sydney"></div>
+        <p>Picturesque Darling Harbour.</p>
+        
+      </div>
+      <div class="col-sm-8">
+        <h2>About Me:</h2>
+        <h5>Title description, Dec 7, 2017</h5>
+        <div class="fakeimg">Fake Image</div>
+        <p>Some text..</p>
+        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        <br>
+      </div>
     </div>
   </div>
-</div>
 
 <div class="jumbotron">
   <footer>
