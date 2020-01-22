f995dbcbf8aa20ee473a27026f42d0ca3dbd194f
emilemoosman <emilemoosman@byui.edu>
2020-01-23 01:37:24 +1100

    add more bootstrap

--- web/index.php
+++ web/index.php
@@ -28,7 +28,7 @@
 
   </nav>
 
-  <div class="container" style="margin-top:30px">
+  <div class="container">
     <div class="row">
       <div class="col-sm-4">
         <h2>Beautiful Sydney:</h2>
@@ -38,17 +38,14 @@
       </div>
       <div class="col-sm-8">
         <h2>About Me:</h2>
-        <h5>Title description, Dec 7, 2017</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        <p>I'm from beautiful Sydney Australia, and will soon be adding my projects.</p>
         <br>
       </div>
     </div>
   </div>
 
-<div class="jumbotron">
-  <footer>
+  <div class="jumbotron">
+    <footer>
       <?php
         date_default_timezone_set("Australia/Sydney");
         echo "Today is " . date("l") . ". ";
